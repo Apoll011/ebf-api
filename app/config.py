e227212ab2086147ce36c65c9ba285cec0afd0cc
Apoll011 <tiagorobotik@gmail.com>
2025-08-08 12:35:44 -0100

    New database

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./database.db"
+    DATABASE_URL: str = "sqlitecloud://cukrrfslhz.g2.sqlite.cloud:8860/ebf?apikey=tVDakyuw8QkDArjv1Anzno9dBWYNJeipgwW50BhKRus"
     SECRET_KEY: str = "your-secret-key"
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60*24*5
     POINT_VALUES: dict = {
         "PRESENCE": 50,
         "BOOK": 20,

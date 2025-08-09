f7c7cfb24c1a91cd35e82d847dca480b2b87bfa2
Apoll0_11 <80171544+Apoll011@users.noreply.github.com>
2025-08-09 15:47:14 -0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -59,7 +59,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 # Student Management
 @app.post("/students", response_model=schemas.StudentResponse, status_code=201)
-def create_student(db: Session = Depends(get_db), current_user: models.User = Depends(dependencies.get_current_user), student: StudentBase):
+def create_student(student: StudentBase, db: Session = Depends(get_db), current_user: models.User = Depends(dependencies.get_current_user)):
     if current_user.role not in ["admin", "teacher"]:
         raise HTTPException(status_code=403, detail="Not enough permissions")
     student = schemas.StudentCreate(**sanitized_body)
